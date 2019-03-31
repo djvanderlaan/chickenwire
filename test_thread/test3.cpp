f620db869351d59d875d94922ff9430f8b6f0aae
djvanderlaan <djvanderlaan@unrealizedtime.nl>
2019-03-31 09:24:36 +0200

    test 3

--- test_thread/test3.cpp
+++ test_thread/test3.cpp
@@ -4,32 +4,31 @@
 #include <mutex>
 #include <functional>
 #include <vector>
+#include <algorithm>
 #include <condition_variable>
 
 std::condition_variable cv;
 std::mutex m;
 int step = 0;
-const int nworkers = 2;
-std::array<int, nworkers> current_step = {0};
+std::vector<int> current_step;
 
 
 void worker_function(int id) {
-  while (true) {
+  bool stop = false;
+  while (!stop) {
     std::unique_lock<std::mutex> lk(m);
     cv.wait(lk, [id]{ return step > current_step[id] || step == -1;});
-    if (step == -1) {
-      current_step[id] = step;
-      cv.notify_all();
-      break;
-    }
+    stop = step == -1;
     lk.unlock();
     // do computation
-    std::cout << id << ": Starting computation\n";
-    std::this_thread::sleep_for(std::chrono::seconds(id+1));
-    std::cout << id << ": Finished computation\n";
+    if (!stop) {
+      std::cout << id << ": Starting computation\n";
+      std::this_thread::sleep_for(std::chrono::seconds(id+1));
+      std::cout << id << ": Finished computation\n";
+    }
     // signal back that we are finished
     lk.lock();
-    current_step[id] += 1;
+    current_step[id] = step;
     lk.unlock();
     cv.notify_all();
   }
@@ -47,18 +46,21 @@ void signal_step(bool stop = false) {
   {
     std::unique_lock<std::mutex> lk(m);
     cv.wait(lk, []{ 
-      bool res = true;
-      for (int i = 0; i < nworkers; ++i) res &= current_step[i] >= step;
-      return res;
+      return std::all_of(current_step.cbegin(), current_step.cend(), 
+        [](int i){ return i == step; });
     });
   }
 }
 
 
 int main(int argc, char* argv[]) {
+  int nworkers = 4;
+
   std::vector<std::thread> workers;
-  for (int i = 0; i < nworkers; ++i) 
+  for (int i = 0; i < nworkers; ++i) {
+    current_step.push_back(0);
     workers.push_back(std::thread(worker_function, i));
+  }
 
   for (int i = 0; i < 2; ++i) {
     signal_step();
