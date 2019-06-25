ff1a402a3cdde90dd44813d6e2bf0c265a64578c
djvanderlaan <jan@knowledge-arcana.nl>
2019-06-25 08:43:11 +0200

    Cleanup shortest path

---
+++ include/shortest_path.h
@@ -0,0 +1,33 @@
+#ifndef shortest_path_h
+#define shortest_path_h
+
+#include "graph.h"
+#include <vector>
+#include <unordered_map>
+
+// Get the length of the shortest path between two nodes
+double shortest_path_length(const Graph& graph, VertexID from, VertexID to);
+
+// Get the shortest path
+struct PathElement {
+  VertexID vertex_id;
+  double path_length;
+};
+typedef std::vector<PathElement> Path;
+Path shortest_path(const Graph& graph, VertexID from, VertexID to);
+
+// Get the lengths of all shortest paths from a node
+typedef std::unordered_map<VertexID, double> PathLengths;
+PathLengths all_shortest_path_lengths(const Graph& graph, VertexID from);
+
+// Diameter of the graph: maximum shortest path
+int diameter(const Graph& graph);
+
+// ==== Connected components
+// Defined in connected_components.cpp
+typedef std::unordered_map<VertexID, VertexID> Components;
+Components connected_components(const Graph& graph);
+
+
+#endif
+
